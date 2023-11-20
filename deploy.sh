b023aa150bdc3326e37912ec3713f866db19ab6d
Viktor Czyzewski <viktor.czyzewski@york.ac.uk>
2023-11-20 13:20:54 +0000

    Made remote URL dynamic in deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 
 name='uoy/lta-webapp'
 file='./docker-lta-webapp-latest.tar'
+remote='langtrackappdev'
 
 sudo docker build -t "$name" .
 sudo docker save -o "$file" "$name"
 sudo chown "$USER:$USER" "$file"
 sudo docker load -i "$file"
-rsync -rva docker-lta-webapp-latest.tar langtrackappdev.york.ac.uk:~/langtrackapp/src/docker-lta-webapp-latest.tar
+rsync -rva docker-lta-webapp-latest.tar "$remote:~/langtrackapp/src/docker-lta-webapp-latest.tar"
